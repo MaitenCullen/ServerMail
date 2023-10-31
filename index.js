6197ff3252258650c8de320c4df820c0b69bd8d9
MaitenCullen <cullen.maiten@gmail.com>
2023-10-31 16:52:32 -0300

    ruta index CON CORS

--- index.js
+++ index.js
@@ -12,29 +12,25 @@ const corsOptions = {
     methods: 'GET, POST, PUT, DELETE, OPTIONS', // Permite todos los métodos HTTP
     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept', // Cabeceras permitidas
   };
-  
-  app.use(cors(corsOptions));
- app.use(bodyparser.json({ extended: true }));
+
+app.use(bodyparser.json({ extended: true }));
 app.use(bodyparser.urlencoded({ extended: true }));
- app.use(require('./routes/mailRoutes'))
-
-
+app.use(cors(corsOptions));
+// app.use(require('./routes/mailRoutes'))
  function requestController(req, res){
     console.log('hola')
     res.send('hola nodejs')
 }
 
-// const method_not_allowed = (req, res) => {
-//     return res.sendStatus(405);
-// };
 app.post('/send', send.sendEmail);
    app.use((req, res, next) => {
       res.header("Access-Control-Allow-Origin", "*");
      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
  });
-
-
+// const method_not_allowed = (req, res) => {
+//     return res.sendStatus(405);
+// };
 
 const server = https.createServer(requestController)
 app.get("/", (req, res) => {
