c34c5d7c96e828160118566baafbb656084280b7
MaitenCullen <cullen.maiten@gmail.com>
2023-10-31 10:29:00 -0300

    cambios server

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const express = require('express');
 const app = express();
 let cors = require('cors');
 const bodyparser =require('body-parser');
-const http = require('http')
+const https = require('https');
 const PORT = process.env.PORT || '80';
 
 app.use(cors({
     origin:'*',
 }));
-app.use((req, res, next) => {
+ app.use((req, res, next) => {
      res.header("Access-Control-Allow-Origin", "*");
      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
      next();
- });
+});
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extend:false}));
 app.use(require('./routes/mailRoutes'))
@@ -21,7 +21,7 @@ function requestController(req, res){
     console.log('hola')
     res.send('hola nodejs')
 }
-const server = http.createServer(requestController)
+const server = https.createServer(requestController)
 app.get("/", (req, res) => {
     const htmlResponse = `
     <html>
