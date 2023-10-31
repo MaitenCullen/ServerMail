54cb891b17c7e2d5e06735168106a05f2f4befea
MaitenCullen <cullen.maiten@gmail.com>
2023-10-31 12:06:08 -0300

    cambios server cors

--- index.js
+++ index.js
@@ -5,32 +5,30 @@ const bodyparser =require('body-parser');
 const https = require('https');
 const PORT = process.env.PORT || '80';
 
- app.use(cors({
-   origin:['https://9-julio-git-pruebanode-maiten-cullen.vercel.app/', 'http://localhost:4200/'],
-   methods:['POST', 'GET', 'OPTIONS']
- }));
-  app.use((req, res, next) => {
-      res.header("Access-Control-Allow-Origin", "*");
-     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-     next();
-});
+ app.use(cors());
+ app.use(bodyparser.json());
+ app.use(bodyparser.urlencoded({extend:false}));
+ app.use(require('./routes/mailRoutes'))
+
+ function requestController(req, res){
+    console.log('hola')
+    res.send('hola nodejs')
+}
 
 const method_not_allowed = (req, res) => {
     return res.sendStatus(405);
 };
+app.post('/send', cors(), (req, res) => { });
+app.all('/send', method_not_allowed);
 
-app.post('/', cors(), (req, res) => { });
-app.all('/', method_not_allowed);
+//   app.use((req, res, next) => {
+//       res.header("Access-Control-Allow-Origin", "*");
+//      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//      next();
+// });
 
 
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extend:false}));
-app.use(require('./routes/mailRoutes'))
 
-function requestController(req, res){
-    console.log('hola')
-    res.send('hola nodejs')
-}
 const server = https.createServer(requestController)
 app.get("/", (req, res) => {
     const htmlResponse = `
