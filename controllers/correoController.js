865f85edcc7694d697eb3251cb2005ae87c51aaf
Maiten <cullen.maiten@gmail.com>
2024-02-18 10:26:00 -0300

    cambie variables de entorno

--- controllers/correoController.js
+++ controllers/correoController.js
@@ -1,17 +1,18 @@
 const {request,response} = require('express');
 const nodeMailer = require('nodemailer');
+require('dotenv').config();
 
 const sendEmail = (req=request, resp=response) => {
 
     let body = req.body;
-    let email = 'info@diagnostico9dejulio.com.ar';
+    let email =  process.env.EMAIL
 
     let config = nodeMailer.createTransport({
         host:'c2172200.ferozo.com',
         port:465,
         auth:{
-            user:'info@diagnostico9dejulio.com.ar',
-            pass:'Reig87Tar/'
+            user: process.env.EMAIL,
+            pass:process.env.PASSWORD
         }
 
     })
